e53447fe46fc97f16075b887c1ccabcc454d5de5
Gunaal-Mahesh <gunaalmahesh@gmail.com>
2025-06-29 02:32:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Sidebar from './components/Sidebar/Sidebar'
+import MainContent from './components/Main/MainContent'
+
+function App() {
+  
+  return (
+    <>
+  <Sidebar/>
+  <MainContent/>
+    </>
+  )
+}
+
+export default App
